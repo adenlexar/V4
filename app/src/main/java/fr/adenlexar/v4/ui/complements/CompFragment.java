82699cf8b0d96750b75b9f899c2a99f152fc6ca8
adenlexar <96285929+adenlexar@users.noreply.github.com>
2022-04-28 22:47:02 +0200

    V4 4eme commit

--- app/src/main/java/fr/adenlexar/v4/ui/complements/CompFragment.java
+++ app/src/main/java/fr/adenlexar/v4/ui/complements/CompFragment.java
@@ -62,36 +62,42 @@ public class CompFragment extends Fragment {
                 }
                 else if(position == 1) {
                     intent.putExtra("TITLE", "Collagène");
-                    intent.putExtra("TEXT", "Le collagène est un composant de notre peau et de nos tendons. Se complémenter en collagène peut s'avérer utile pour prévenir" +
+                    intent.putExtra("TEXT", "Le collagène est un composant de notre peau et de nos tendons. Se complémenter en collagène peut s'avérer utile pour prévenir " +
                             "les blessures articulaires telles que les tendinites. Il peut se consommer en poudre ou en gélulle.");
                 }
                 else if(position == 2) {
                     intent.putExtra("TITLE", "Vitamine D");
-                    intent.putExtra("TEXT", "");
+                    intent.putExtra("TEXT", "La vitamine D est un micronutriment essentiel au bon fonctionnement du corps humain (permet la fixation du calcium et autre)."+
+                            "On la retrouve dans les poissons gras mais on peut aussi en synthétiser naturellement en s'exposant au soleil. Or en hiver on se retrouve souvent en situtation de carence et se complémenter peut alors être une bonne idée. +" +
+                            " On déconseille les cures de vitamine D prescrites par les médecins car celles-ci surdosent les besoins journaliers et ne durent qu'un mois. On préférera un apport journalier d'environ 3 microgrammes.");
                 }
                 else if(position == 3) {
                     intent.putExtra("TITLE", "Gainer");
-                    intent.putExtra("TEXT", "");
+                    intent.putExtra("TEXT", "Le gainer est un complément alimentaire utilisé par les personnes en prise de masse musculaire. Il permet d'augmenter son nombre de kilocalories" +
+                            " journalier et ce sous forme d'une poudre contenant glucides et protéines." +" On déconseille les gainer trop riches en sucres rapides car ceux-ci sont moins bon pour la santé que ceux à base de "+
+                            "sucre lent comme la poudre d'avoine par exemple.");
                 }
                 else if(position == 4) {
                     intent.putExtra("TITLE", "Pré-Workout");
-                    intent.putExtra("TEXT", "");
+                    intent.putExtra("TEXT", "Le pré-workout est un complément à prendre avant sa séance de sport. Souvent à base de caffeine, il permet un regain d'énergie pour ne pas se sentir fatigué à l'entraînement."+
+                            "Il ne faut pas trop en consommer car la caffeine à haute dose peut être dangereux pour la santé.");
                 }
                 else if(position == 5) {
                     intent.putExtra("TITLE", "Créatine");
-                    intent.putExtra("TEXT", "");
+                    intent.putExtra("TEXT", "La créatine est un complément possédant de nombreuses vertues : " +
+                            " amélioration de la synthèse des protéines et du glycogène, renforcement du système immunitaire, augmentation de la force, des fonctions cérébrales etc. On recommande d'en prendre environ 2g par jour.");
                 }
                 else if(position == 6) {
                     intent.putExtra("TITLE", "Zinc");
-                    intent.putExtra("TEXT", "");
+                    intent.putExtra("TEXT", "Le zinc favorise le processus de cicatrisation des plaies et aide à lutter contre les problèmes de peau. Il agit en tant qu'antioxydant en neutralisant les radicaux libres, protège les cellules de la peau, et participe à la pousse des cheveux et des ongles.");
                 }
                 else if(position == 7) {
                     intent.putExtra("TITLE", "Magnésium");
-                    intent.putExtra("TEXT", "");
+                    intent.putExtra("TEXT", "Il est conseillé pour améliorer l'endurance, les performances chez les sportifs, et l'asthme. Il est utile également pour traiter les crampes musculaires, diminuer la fatigue, le stress, l'anxiété, l'insomnie");
                 }
                 else if(position == 8) {
                     intent.putExtra("TITLE", "Spiruline");
-                    intent.putExtra("TEXT", "");
+                    intent.putExtra("TEXT", "Puissant antioxydant, la spiruline aide à soutenir les fonctions d'élimination de notre organisme et à lutter contre la formation des radicaux libres responsables du stress oxydatif. ");
                 }
 
                 startActivity(intent);
